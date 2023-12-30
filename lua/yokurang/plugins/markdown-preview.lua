97b5336e48c7985810895ff3f5a0b858b1ba20c6
You Guang <yokurang@gmail.com>
2023-12-30 14:19:27 +0700

    markdown preview

--- lua/yokurang/plugins/markdown-preview.lua
+++ lua/yokurang/plugins/markdown-preview.lua
@@ -4,14 +4,15 @@ return {
   ft = { "markdown" },
   build = function() vim.fn["mkdp#util#install"]() end,
   config = function()
-    vim.g.mkdp_auto_start = 1
-    vim.g.mkdp_auto_close = 1
-    vim.g.mkdp_refresh_slow = 1
-    vim.g.mkdp_open_to_the_world = 1
+  vim.g.mkdp_auto_start = 1
+  vim.g.mkdp_auto_close = 1
+  vim.g.mkdp_refresh_slow = 1
+  vim.g.mkdp_open_to_the_world = 1
 
-    keymap.set({
-      { "n", "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>" },
-      { "n", "<leader>ms", "<cmd>MarkdownPreviewStop<cr>" },
-    })
+  local keymap = vim.keymap
+  keymap.set({
+    { "n", "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>" },
+    { "n", "<leader>ms", "<cmd>MarkdownPreviewStop<cr>" },
+  })
   end,
 }
